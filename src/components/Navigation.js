756ac0897a33bfdbd7251f3719d0dd3f7e131198
Ali Fahed <alifahed67@gmail.com>
2022-09-09 16:31:17 +0800

    Update navigation style

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,21 +5,23 @@ const Navigation = () => {
   return (
     <div>
       <header className="header">
-        <div className="container">
-          <h1 className="company-name">StoreCom</h1>
+        <div className="header-container">
+          <div className="container">
+            <h1 className="company-name">StoreCom</h1>
+          </div>
+          <nav className="navbar">
+            <li className="nav-links">
+              <Link className="links" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-links">
+              <Link className="links" to="/shop">
+                Store
+              </Link>
+            </li>
+          </nav>
         </div>
-        <nav className="navbar">
-          <li className="nav-links">
-            <Link className="links" to="/">
-              Home
-            </Link>
-          </li>
-          <li className="nav-links">
-            <Link className="links" to="/shop">
-              Store
-            </Link>
-          </li>
-        </nav>
       </header>
     </div>
   );
