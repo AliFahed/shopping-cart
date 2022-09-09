1d206e24eeb658f4ff579b7d62e8bb338afeeb14
Ali Fahed <alifahed67@gmail.com>
2022-09-09 16:40:40 +0800

    Add products details

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,7 +1,31 @@
+import "../styles/shop.css";
+import GreyNike from "../images/grey_nike.jpg";
+
 const Shop = () => {
   return (
     <div>
-      <h1>Shop!</h1>
+      <main className="main">
+        <section className="products">
+          <div className="items">
+            <img className="item-image" src={GreyNike} alt="Grey Nike" />
+            <p className="item-name">Grey Nike</p>
+            <p className="item-price">$49.99</p>
+            <button className="add-item-btn">Add to Cart</button>
+          </div>
+          <div className="items">
+            <img className="item-image" src={GreyNike} alt="Grey Nike" />
+            <p className="item-name">Grey Nike</p>
+            <p className="item-price">$49.99</p>
+            <button className="add-item-btn">Add to Cart</button>
+          </div>
+          <div className="items">
+            <img className="item-image" src={GreyNike} alt="Grey Nike" />
+            <p className="item-name">Grey Nike</p>
+            <p className="item-price">$49.99</p>
+            <button className="add-item-btn">Add to Cart</button>
+          </div>
+        </section>
+      </main>
     </div>
   );
 };
