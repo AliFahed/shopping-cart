0cc5659372ac5929307cb09f015a574cbf2829ca
Ali Fahed <alifahed67@gmail.com>
2022-09-09 20:37:42 +0800

    Add and style home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,28 @@
+import "../styles/home.css";
+import NikeImage from "../images/nike_home_page_image.jpg";
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <div>
-      <h1>Welcome Home!</h1>
+      <section className="hero">
+        <div className="hero-section">
+          <div className="hero-container">
+            <div className="text-container">
+              <p className="sub-text">Best Nike Shoe collections</p>
+              <h2 className="main-text">#JUST DO IT!</h2>
+              <button className="shop-now-btn">
+                <Link to="/shop" className="shop-now-link">
+                  Shop Now
+                </Link>
+              </button>
+            </div>
+            <div className="image-container">
+              <img className="nike-image" src={NikeImage} alt="Nike Shoes" />
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
