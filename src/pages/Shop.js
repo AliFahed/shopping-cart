e27ff6c41df9107a31928a59d12e97fc22123779
Ali Fahed <alifahed67@gmail.com>
2022-09-11 15:26:34 +0800

    Add shopping cart: view items container

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -3,12 +3,32 @@ import "../styles/shop.css";
 import { productsData } from "../productsData";
 import Product from "../components/Product";
 import ShoppingBag from "../images/shopping-bag.jpg";
+import "../styles/viewItems.css";
 
 const Shop = () => {
   const [addItem, setAddItem] = React.useState(0);
 
+  // below function is passed to Product.js Component
   const handleAddToCartBtn = () => {
     setAddItem((prevItem) => prevItem + 1);
+  };
+
+  const viewItems = () => {
+    const hideContainer = document.querySelector(
+      ".hide-shopping-cart-container"
+    );
+
+    hideContainer.classList.add("view-shopping-cart-container");
+    hideContainer.classList.remove("hide-shopping-cart-container");
+  };
+
+  const hideItems = () => {
+    const viewContainer = document.querySelector(
+      ".view-shopping-cart-container"
+    );
+
+    viewContainer.classList.add("hide-shopping-cart-container");
+    viewContainer.classList.remove("view-shopping-cart-container");
   };
 
   const products = productsData.map((item) => {
@@ -25,9 +45,36 @@ const Shop = () => {
     <div>
       <main className="main">
         <div className="shopping-cart-container">
-          <img className="shopping-cart" src={ShoppingBag} alt="shopping bag" />
+          <img
+            className="shopping-cart"
+            src={ShoppingBag}
+            alt="shopping bag"
+            onClick={viewItems}
+          />
           <div className="items-count">{addItem}</div>
         </div>
+
+        <div className="hide-shopping-cart-container">
+          <div className="items-container">
+            <div className="wrapper">
+              <h2 className="title">Your Shopping Cart</h2>
+              <div className="selected-items-container">
+                here should have any item when press add to cart btn
+              </div>
+              <div className="items-details-container">
+                <p className="items-number">No. of Items: {addItem}</p>
+                <p className="total-amount">Total amount: $100</p>
+              </div>
+              <div className="buttons-container">
+                <button className="back-btn" onClick={hideItems}>
+                  Back to Shopping
+                </button>
+                <button className="proceed-btn">Procedd to Payment</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <section className="products">{products}</section>
       </main>
     </div>
