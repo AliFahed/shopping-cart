06b652864b5b1cfb4978f0dc9915a6abd5992400
Ali Fahed <alifahed67@gmail.com>
2022-10-12 11:26:19 +0800

    Refactor react-router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import "./styles/main.css";
@@ -6,13 +6,13 @@ import Navigation from "./components/Navigation";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Navigation />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/shop" element={<Shop />} />
       </Routes>
-    </>
+    </Router>
   );
 };
 
